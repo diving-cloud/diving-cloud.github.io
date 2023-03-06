2af277a34e12b3e77abbd3f6a9bb010c802b1a7a
hezongkui <hezongkui@kuaishou.com>
2023-03-06 11:52:42 +0800

    fixbase

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { themeConfig } from "./themeConfig";
 import { searchPlugin } from "@vuepress/plugin-search";
 
 export default defineUserConfig({
-  base: "/docs/",
+  base: "/",
   theme: themeConfig,
   shouldPrefetch: false,
   title: "JavaGuide(Java面试+学习指南)",
