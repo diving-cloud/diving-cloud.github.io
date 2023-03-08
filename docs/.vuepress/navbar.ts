44243a6aaf733e94304dc0ad8d649cc6f927212d
hezongkui <zongkuihe@gmail.com>
2023-03-08 23:45:57 +0800

    改顶部导航栏标题

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,7 +1,7 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const navbarConfig = navbar([
-  { text: "面试指南", icon: "java", link: "/home.md" },
+  { text: "学习指南", icon: "java", link: "/home.md" },
   { text: "开源项目", icon: "github", link: "/open-source-project/" },
   { text: "技术书籍", icon: "book", link: "/books/" },
   { text: "技术文章", icon: "article", link: "/high-quality-technical-articles/" },
